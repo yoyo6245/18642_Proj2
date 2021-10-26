f549f5781b9c4188175d00850545e200c7127ac3
yoyo6245 <yoyo6245a@gmail.com>
2021-10-26 01:30:21 -0400

    build.sh done

--- yuanyul_build.sh
+++ yuanyul_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-maze_num=""
-echo "$1"
-maze_num=m$1.maze
-echo $maze_num
-./build_run_turtle.sh $maze_num
+cd ~/catkin_ws
+source ~/catkin_ws/devel/setup.bash
+catkin_make ece642rtle_student
+catkin_make ece642rtle_node
+cd ~/catkin_ws/src/ece642rtle
