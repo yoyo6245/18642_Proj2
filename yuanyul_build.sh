fb779fb10bdd5c33b04e924d930952f87eca0d5d
yoyo6245 <yoyo6245a@gmail.com>
2021-10-26 01:37:04 -0400

    build.sh done2

--- yuanyul_build.sh
+++ yuanyul_build.sh
@@ -2,5 +2,8 @@
 cd ~/catkin_ws
 source ~/catkin_ws/devel/setup.bash
 catkin_make ece642rtle_student
+source ~/catkin_ws/devel/setup.bash
 catkin_make ece642rtle_node
+source ~/catkin_ws/devel/setup.bash
+catkin_make ece642rtle_student
 cd ~/catkin_ws/src/ece642rtle
